d270583561656de53c3b900079499083a4310e6c
ProgrammerAuditore <b4rce3l0n4@gmail.com>
2020-12-02 16:32:39 -0600

    *** Se implemento el boton de generar reporte y se agrego el metodo para eliminar datos de la base de datos de Mysql.

--- src/simuladordado/Ventana.java
+++ src/simuladordado/Ventana.java
@@ -8,6 +8,7 @@ package simuladordado;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -18,6 +19,12 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 import java.sql.*;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.util.JRLoader;
+import net.sf.jasperreports.view.JasperViewer;
 
 /**
  *
@@ -107,6 +114,11 @@ public class Ventana extends javax.swing.JFrame {
         btnInforme.setBackground(new java.awt.Color(255, 204, 0));
         btnInforme.setForeground(new java.awt.Color(255, 255, 255));
         btnInforme.setText("Informe");
+        btnInforme.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseReleased(java.awt.event.MouseEvent evt) {
+                btnInformeMouseReleased(evt);
+            }
+        });
 
         panelImgCaras.setBorder(javax.swing.BorderFactory.createEtchedBorder());
 
@@ -201,90 +213,36 @@ public class Ventana extends javax.swing.JFrame {
         
         tiempo.addActionListener(tarea);
         tiempo.start();
+        this.Run = true;
      
     }//GEN-LAST:event_bntIniciarMouseReleased
 
     private void btnGuardarMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnGuardarMousePressed
-        // TODO add your handling code here:
-        tiempo.stop();
         
-        try{
+        // * Si el programa ya se ha inicializado se guardan los datos
+        // de lo contrario marca un error....
+        if( this.Run ){
             
-            for( int item=0; item < 6; item++ ){
-                Connection connect = DriverManager.getConnection("jdbc:mysql://localhost/bdva", "root", "");
-                PreparedStatement query = connect.prepareStatement("INSERT into tblva VALUES(?,?,?,?,?)");
-                
-                query.setString(1, "0" );
-                query.setString(2, this.tblResultado.getValueAt(item, 0).toString().trim() );
-                query.setString(3, this.tblResultado.getValueAt(item, 1).toString().trim() );
-                query.setString(4, this.tblResultado.getValueAt(item, 2).toString().trim() );
-                query.setString(5, this.tblResultado.getValueAt(item, 3).toString().trim() );
-                
-                query.executeUpdate();
-            }
+            // Guardar los datos a la base de datos
+            this.fncEliminarDatos();
+            this.fncGuardarDatos();
+        
+        }else{
             
-            System.out.println("Datos registrados...");
-            
-        }catch(Exception e){
-            System.out.println("Error de mysql : " + e.getMessage());
+            JOptionPane.showMessageDialog(null, "Lo siento, el programa aun no ha iniciado");
+        
         }
         
     }//GEN-LAST:event_btnGuardarMousePressed
-    
-    private void fncContador(int cara){      
-        
-        for(int item = 0; item < 6; item++){
-            
-            if( this.tblResultado.getValueAt(item,1).toString().isEmpty()){
-                this.tblResultado.setValueAt(0,item,1);
-            }else if( this.tblResultado.getValueAt(item,2).toString().isEmpty()){
-                this.tblResultado.setValueAt(0,item,2);
-            }
-            
-            // Contador de caras
-            if( cara == (item + 1) ) {
-                this.tblResultado.setValueAt( Integer.parseInt(this.tblResultado.getValueAt(item ,1).toString()) + 1, item, 1);
-            }
-            
-            // Frecuencia relativa
-            double parse = Double.parseDouble(this.tblResultado.getValueAt(item ,1).toString()) / this.intTotalDeResultados;
-            this.tblResultado.setValueAt( String.format("%.2f", parse ) , item, 2);
 
-        }
+    private void btnInformeMouseReleased(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnInformeMouseReleased
         
-        // Frecuencia relativa acumulada
-        this.tblResultado.setValueAt( String.format("%.2f", fncSumas(0) ), 0, 3);
-        this.tblResultado.setValueAt( String.format("%.2f", fncSumas(1) ), 1, 3);
-        this.tblResultado.setValueAt( String.format("%.2f", fncSumas(2) ), 2, 3);
-        this.tblResultado.setValueAt( String.format("%.2f", fncSumas(3) ), 3, 3);
-        this.tblResultado.setValueAt( String.format("%.2f", fncSumas(4) ), 4, 3);
-        this.tblResultado.setValueAt( String.format("%.2f", fncSumas(5) ), 5, 3);
-
-    }
-    
-    private double fncSumas(int items){
-        double suma = 0;
-        for(int item = 0; item <= items; item++){
-            suma += Double.parseDouble(this.tblResultado.getValueAt(item ,2).toString());
-        }
-        return suma;
-    }
-    
-    private void fncInsertarPicture(JPanel contenedor, String url, boolean vaciar){
+            
+        // Generar el reporte de los datos a la base de datos.
+        this.ReporteVA();
         
-        if(vaciar) contenedor.removeAll();
-        
-        ImageIcon icono = new ImageIcon( getClass().getResource(url) );
-        JLabel etiquetaImagen = new JLabel();
-        etiquetaImagen.setBounds(0, 0, contenedor.getWidth(), contenedor.getHeight());
-        etiquetaImagen.setIcon( new ImageIcon(icono.getImage().getScaledInstance(etiquetaImagen.getWidth(), etiquetaImagen.getHeight(), Image.SCALE_SMOOTH)) );
-        contenedor.add(etiquetaImagen);
-        
-        if(vaciar) contenedor.validate();
-        if(vaciar) contenedor.repaint();
-    }
-    
-    
+    }//GEN-LAST:event_btnInformeMouseReleased
+     
     /**
      * @param args the command line arguments
      */
@@ -337,4 +295,128 @@ public class Ventana extends javax.swing.JFrame {
    private int intTotalDeResultados;
    private ActionListener escucha;
    private Timer tiempo = new Timer(1000, escucha);
+   private boolean Run = false;
+   
+   public void ReporteVA(){
+        File master=null;
+        try {
+            master = new File(getClass().getResource("rptva.jasper").toURI());
+        }
+        catch (Exception ex)
+        {
+            ex.printStackTrace();
+            JOptionPane.showMessageDialog(null, "No se puede abrir el archivo ó No existe en esta ruta","Error",JOptionPane.ERROR_MESSAGE);
+        }
+        try{
+            JasperReport masterReport = null;
+            try{
+                masterReport = (JasperReport) JRLoader.loadObject(master);
+            }
+            catch (JRException e){
+                System.out.println("Error al cargar el archivo de reporte maestro: " + e.getMessage());
+            }
+            JasperPrint jasperPrint = JasperFillManager.fillReport(masterReport,null, DriverManager.getConnection("jdbc:mysql://localhost/bdva", "root", "") );
+            JasperViewer jviewer = new JasperViewer(jasperPrint,false);
+            jviewer.setTitle("Reporte de usuarios");
+            jviewer.setVisible(true);
+        }
+        catch (Exception j){
+            System.out.println("Mensaje de Error:"+j.getMessage());
+        }
+    }
+   
+   private void fncContador(int cara){      
+        
+        for(int item = 0; item < 6; item++){
+            
+            if( this.tblResultado.getValueAt(item,1).toString().isEmpty()){
+                this.tblResultado.setValueAt(0,item,1);
+            }else if( this.tblResultado.getValueAt(item,2).toString().isEmpty()){
+                this.tblResultado.setValueAt(0,item,2);
+            }
+            
+            // Contador de caras
+            if( cara == (item + 1) ) {
+                this.tblResultado.setValueAt( Integer.parseInt(this.tblResultado.getValueAt(item ,1).toString()) + 1, item, 1);
+            }
+            
+            // Frecuencia relativa
+            double parse = Double.parseDouble(this.tblResultado.getValueAt(item ,1).toString()) / this.intTotalDeResultados;
+            this.tblResultado.setValueAt( String.format("%.2f", parse ) , item, 2);
+
+        }
+        
+        // Frecuencia relativa acumulada
+        this.tblResultado.setValueAt( String.format("%.2f", fncSumas(0) ), 0, 3);
+        this.tblResultado.setValueAt( String.format("%.2f", fncSumas(1) ), 1, 3);
+        this.tblResultado.setValueAt( String.format("%.2f", fncSumas(2) ), 2, 3);
+        this.tblResultado.setValueAt( String.format("%.2f", fncSumas(3) ), 3, 3);
+        this.tblResultado.setValueAt( String.format("%.2f", fncSumas(4) ), 4, 3);
+        this.tblResultado.setValueAt( String.format("%.2f", fncSumas(5) ), 5, 3);
+
+    }
+    
+    private double fncSumas(int items){
+        double suma = 0;
+        for(int item = 0; item <= items; item++){
+            suma += Double.parseDouble(this.tblResultado.getValueAt(item ,2).toString());
+        }
+        return suma;
+    }
+    
+    private void fncInsertarPicture(JPanel contenedor, String url, boolean vaciar){
+        
+        if(vaciar) contenedor.removeAll();
+        
+        ImageIcon icono = new ImageIcon( getClass().getResource(url) );
+        JLabel etiquetaImagen = new JLabel();
+        etiquetaImagen.setBounds(0, 0, contenedor.getWidth(), contenedor.getHeight());
+        etiquetaImagen.setIcon( new ImageIcon(icono.getImage().getScaledInstance(etiquetaImagen.getWidth(), etiquetaImagen.getHeight(), Image.SCALE_SMOOTH)) );
+        contenedor.add(etiquetaImagen);
+        
+        if(vaciar) contenedor.validate();
+        if(vaciar) contenedor.repaint();
+    }
+    
+    private void fncGuardarDatos(){
+        
+        // TODO add your handling code here:
+        tiempo.stop();
+        
+        try{
+            
+            for( int item=0; item < 6; item++ ){
+                Connection connect = DriverManager.getConnection("jdbc:mysql://localhost/bdva", "root", "");
+                PreparedStatement query = connect.prepareStatement("INSERT into `tblva` VALUES(?,?,?,?,?)");
+                
+                query.setString(1, "0" );
+                query.setString(2, this.tblResultado.getValueAt(item, 0).toString().trim() );
+                query.setString(3, this.tblResultado.getValueAt(item, 1).toString().trim() );
+                query.setString(4, this.tblResultado.getValueAt(item, 2).toString().trim() );
+                query.setString(5, this.tblResultado.getValueAt(item, 3).toString().trim() );
+                
+                query.executeUpdate();
+            }
+            
+            System.out.println("Datos registrados...");
+            
+        }catch(Exception e){
+            System.out.println("Error de mysql : " + e.getMessage());
+        }    
+    
+    }
+    
+    private void fncEliminarDatos(){
+        
+        try {
+            
+            Connection conexion = DriverManager.getConnection("jdbc:mysql://localhost/bdva", "root", "");
+            PreparedStatement query = conexion.prepareStatement("DELETE FROM `tblva`");
+            query.executeUpdate();
+            
+        } catch (Exception e) {
+        }
+        
+    }
+    
 }
